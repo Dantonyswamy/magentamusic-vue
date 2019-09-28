5129cdf9bab4a9faafd62b50019fe59299bc82a5
Dennis Antonyswamy <irwindenni@gmail.com>
2019-09-28 12:33:47 -0400

    This version works
    Fixed some variable and adjusted the plugin path. The template is now working

--- src/components/music.vue
+++ src/components/music.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-    <v-cols cols="12">
+    <v-col cols="12">
       <v-row align="center" justify="center">
         <v-card class="mx-auto" max-width="400">
           <v-card-text>
@@ -23,21 +23,64 @@
           </v-card-actions>
         </v-card>
       </v-row>
-    </v-cols>
+    </v-col>
   </v-container>
 </template>
 <script>
-//import { player } from "./plugins/magenta"; -- this import statement is not working
-import { core } from "@magenta/music/node/core";
+import { player, model } from "@/plugins/magenta";
 export default {
-  methods: {
-    play() {
-      const globalAny = global;
-      globalAny.performance = Date;
-      globalAny.fetch = require("node-fetch");
-
-      const player = new core.Player();
-      var TWINKLE_TWINKLE = {
+  data() {
+    return {
+      //musical sequence variables to be passed to the model
+      sequence: {
+        ticksPerQuarter: 220,
+        totalTime: 28.5,
+        timeSignatures: [
+          {
+            time: 0,
+            numerator: 4,
+            denominator: 4
+          }
+        ],
+        tempos: [
+          {
+            time: 0,
+            qpm: 120
+          }
+        ],
+        notes: [
+          { pitch: "Gb4", startTime: 0, endTime: 1 },
+          { pitch: "F4", startTime: 1, endTime: 3.5 },
+          { pitch: "Ab4", startTime: 3.5, endTime: 4 },
+          { pitch: "C5", startTime: 4, endTime: 4.5 },
+          { pitch: "Eb5", startTime: 4.5, endTime: 5 },
+          { pitch: "Gb5", startTime: 5, endTime: 6 },
+          { pitch: "F5", startTime: 6, endTime: 7 },
+          { pitch: "E5", startTime: 7, endTime: 8 },
+          { pitch: "Eb5", startTime: 8, endTime: 8.5 },
+          { pitch: "C5", startTime: 8.5, endTime: 9 },
+          { pitch: "G4", startTime: 9, endTime: 11.5 },
+          { pitch: "F4", startTime: 11.5, endTime: 12 },
+          { pitch: "Ab4", startTime: 12, endTime: 12.5 },
+          { pitch: "C5", startTime: 12.5, endTime: 13 },
+          { pitch: "Eb5", startTime: 13, endTime: 14 },
+          { pitch: "D5", startTime: 14, endTime: 15 },
+          { pitch: "Db5", startTime: 15, endTime: 16 },
+          { pitch: "C5", startTime: 16, endTime: 16.5 },
+          { pitch: "F5", startTime: 16.5, endTime: 17 },
+          { pitch: "F4", startTime: 17, endTime: 19.5 },
+          { pitch: "G4", startTime: 19.5, endTime: 20 },
+          { pitch: "Ab4", startTime: 20, endTime: 20.5 },
+          { pitch: "C5", startTime: 20.5, endTime: 21 },
+          { pitch: "Eb5", startTime: 21, endTime: 21.5 },
+          { pitch: "C5", startTime: 21.5, endTime: 22 },
+          { pitch: "Eb5", startTime: 22, endTime: 22.5 },
+          { pitch: "C5", startTime: 22.5, endTime: 24.5 },
+          { pitch: "Eb5", startTime: 24.5, endTime: 25.5 },
+          { pitch: "G4", startTime: 25.5, endTime: 28.5 }
+        ]
+      },
+      TWINKLE_TWINKLE: {
         notes: [
           { pitch: 60, startTime: 0.0, endTime: 0.5 },
           { pitch: 60, startTime: 0.5, endTime: 1.0 },
@@ -61,8 +104,18 @@ export default {
           }
         ],
         totalTime: 8
-      };
-      player.start(TWINKLE_TWINKLE);
+      }
+    };
+  },
+  methods: {
+    play() {
+      model
+        .initialize()
+        .then(() => model.sample(1))
+        .then(samples => {
+          player.resumeContext();
+          player.start(samples[0]);
+        });
     }
   }
 };
--- src/plugins/magenta.js
+++ src/plugins/magenta.js
@@ -1,9 +1,14 @@
+const mm = require("@magenta/music/node/music_vae");
 const core = require("@magenta/music/node/core");
 
 const globalAny = global;
 globalAny.performance = Date;
 globalAny.fetch = require("node-fetch");
 
+const model = new mm.MusicVAE(
+  "https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small"
+);
 const player = new core.Player();
 
 export { player };
+export { model };
