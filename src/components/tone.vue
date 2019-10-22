d0b1637450d651a7df43cc3f071d1e93f4f7bbad
Dennis Antonyswamy <irwindenni@gmail.com>
2019-10-21 22:36:11 -0400

    Schedule Repeat feature tryout
    Tried out schedule feature by calling reapeat method from created component

--- src/components/tone.vue
+++ src/components/tone.vue
@@ -13,12 +13,15 @@
             </p>
             <div class="text--primary">
               click the play button to hear .<br />
-              notes C4,E4,G4,B4
+              notes: ["C2", "Eb2", "G2", "C3", "Eb3", "G3"]
             </div>
           </v-card-text>
           <v-card-actions>
-            <v-btn text color="green" @click="playtone">
+            <v-btn text color="green" @click="startSynth">
               Play
+            </v-btn>
+            <v-btn text color="red" @click="stopSynth">
+              Stop
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -28,16 +31,33 @@
 </template>
 <script>
 import Tone from "tone";
+
 export default {
   data() {
     return {
-      synth: ""
+      index: 0,
+      notes: ["C2", "Eb2", "G2", "C3", "Eb3", "G3"],
+      synth: new Tone.Synth(),
+      filter: new Tone.Filter(1000, "lowpass", -24),
+      filterEnv: new Tone.FrequencyEnvelope(0.3, 0.2, 0.01, 0.8)
     };
   },
   created() {
-    this.synth = new Tone.Synth().toMaster();
-    Tone.Transport.bpm.value = 400;
+    // this.synth = new Tone.MembraneSynth().toMaster();
+    // Tone.Transport.bpm.value = 120;
+
+    Tone.Transport.scheduleRepeat(time => {
+      this.repeat(time);
+      // console.log(time)
+      // console.log(Tone.Transport.bpm.value)
+    }, "4n");
+    Tone.Transport.bpm.value = 200;
+    this.synth.oscillator.type = "square";
+    this.filterEnv.connect(this.filter.frequency);
+    this.synth.connect(this.filter);
+    this.filter.toMaster();
   },
+
   methods: {
     playtone() {
       this.synth.triggerAttackRelease("C4", "4n", "8n");
@@ -63,16 +83,32 @@ export default {
     simple() {
       this.synth.triggerAttackRelease("C4", "8n");
     },
-    melody() {
-      Tone.Transport.schedule(this.synth.triggerAttackRelease("C2", "8n", 0));
-      Tone.Transport.schedule(
-        this.synth.triggerAttackRelease("C2", "8n", "0:2")
-      );
-      Tone.Transport.schedule(
-        this.synth.triggerAttackRelease("C2", "8n", "0:2:2.5")
-      );
+    note(time) {
+      this.synth.triggerAttackRelease("C2", "8n", time);
+    },
+    schedule: function() {
+      Tone.Transport.schedule(this.note(0));
+      Tone.Transport.schedule(this.note("0:2"));
+      Tone.Transport.schedule(this.note("0:2:2.5"));
       Tone.Transport.loopEnd = "1m";
       Tone.Transport.loop = true;
+    },
+
+    repeat(time) {
+      let note = this.notes[this.index % this.notes.length];
+      // (note, duration, time, velocity)
+      // the note is the pitch, duration is how long to hold the
+      // note in question before triggering the release of the
+      // amplitude enveleope (ie. the release of the note)
+      this.synth.triggerAttackRelease(note, "16n", time);
+      this.filterEnv.triggerAttackRelease();
+      this.index++;
+    },
+    startSynth() {
+      Tone.Transport.start();
+    },
+    stopSynth() {
+      Tone.Transport.stop();
     }
   }
 };
